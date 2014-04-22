ee7232bbc4282b1aa35c6a5297c007a0820f584e
John Good <Judiconis@gmail.com>
2014-04-22 09:30:38 +0100

    Changed XRay list to use strings. Now uses less code.

--- minecraft/com/arachnias/wolfpack/mods/XRay.java
+++ minecraft/com/arachnias/wolfpack/mods/XRay.java
@@ -13,13 +13,13 @@ import com.arachnias.wolfpack.modulebase.Module;
 
 public class XRay extends Module {
 	
-	private static ArrayList<Block> xRayList = new ArrayList<Block>();
+	private static ArrayList<String> xRayList = new ArrayList<String>();
 	
 	public XRay() {
 		super("XRay", Keyboard.KEY_X, new String[] {"xray"});
 		this.setModuleColour(0x00FBFF);
 		
-		xRayList.add(Block.getBlockFromName("diamond_ore"));
+		xRayList.add("diamond_ore");
 	}
 	
 	@Override
@@ -38,14 +38,21 @@ public class XRay extends Module {
 		if (commandwithArgs.length == 1) {
 			// Toggle the module
 			this.toggle();
+		} else if (commandwithArgs[1].equalsIgnoreCase("list")) {
+			String list = "Active XRay blocks: ";
+			for (String blockName: xRayList) {
+				list += blockName + ", ";
+			}
+			this.mc.thePlayer.addChatMessage(new ChatComponentText(list));
 		} else if (commandwithArgs[1].equalsIgnoreCase("add")) {
 			if (commandwithArgs.length < 3) {
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Not enough parameters. Usage: .xray add block_name"));
+			} else if (xRayList.contains(commandwithArgs[2])) { 
+				this.mc.thePlayer.addChatMessage(new ChatComponentText("Block already in XRay List!"));
 			} else if (Block.getBlockFromName(commandwithArgs[2]) == null) {
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Invalid block name!"));
 			} else {
-				Block block = Block.getBlockFromName(commandwithArgs[2]);
-				xRayList.add(block);
+				xRayList.add(commandwithArgs[2]);
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Added " + commandwithArgs[2] + " to XRay list!"));
 				this.mc.renderGlobal.setWorldAndLoadRenderers(this.mc.theWorld);
 			}
@@ -54,18 +61,17 @@ public class XRay extends Module {
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Not enough parameters. Usage: .xray remove block_name"));
 			} else if (Block.getBlockFromName(commandwithArgs[2]) == null) {
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Invalid block name!"));
-			} else if (!xRayList.contains(Block.getBlockFromName(commandwithArgs[2]))) { 
+			} else if (!xRayList.contains(commandwithArgs[2])) { 
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Block not found in XRay List!"));
 			} else {
-				Block block = Block.getBlockFromName(commandwithArgs[2]);
 				this.mc.thePlayer.addChatMessage(new ChatComponentText("Removed " + commandwithArgs[2] + " from XRay list!"));
 				this.mc.renderGlobal.setWorldAndLoadRenderers(this.mc.theWorld);
-				xRayList.remove(block);
+				xRayList.remove(commandwithArgs[2]);
 			}
 		}
 	}
 	
-	public static ArrayList<Block> getXRayList() {
+	public static ArrayList<String> getXRayList() {
 		return xRayList;
 	}
 
